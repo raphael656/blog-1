8e61de132ade908b31770f2b8d7408dd337a9130
raphael656 <74969464+raphael656@users.noreply.github.com>
2021-11-24 08:21:24 +0100

    begin exo 1

---
+++ src/Entity/Contact.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ContactRepository::class)
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Sujet;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Message;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Newsletter;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSujet(): ?string
+    {
+        return $this->Sujet;
+    }
+
+    public function setSujet(string $Sujet): self
+    {
+        $this->Sujet = $Sujet;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->Message;
+    }
+
+    public function setMessage(string $Message): self
+    {
+        $this->Message = $Message;
+
+        return $this;
+    }
+
+    public function getNewsletter(): ?string
+    {
+        return $this->Newsletter;
+    }
+
+    public function setNewsletter(string $Newsletter): self
+    {
+        $this->Newsletter = $Newsletter;
+
+        return $this;
+    }
+}
